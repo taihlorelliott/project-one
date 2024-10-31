cd42a34acc73a6c11238c43ac3452f20af5f3ac1
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-10-31 16:19:29 -0400

    added if else

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const cookieButton = document.getElementById(`cookie`)
-console.dir(cookieButton)
+const winnerText = document.getElementById(`winner`)
+
+winnerText.style.display = "none"
 
 let count = 0;
 
 const clickCounter = () => {
     count++;
-    document.getElementById(`display`).innerText = `${count} Cookies!`
+    document.getElementById(`display`).innerText = `${count}`
+if (count === 6) {
+    winnerText.style.display = "flex"
+    winnerText.style.justifyContent = "center"
+    cookieButton.style.display = "none"
+}
 }
 
 cookieButton.addEventListener(`click`, clickCounter)
