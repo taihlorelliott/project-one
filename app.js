fd8097ffd9c42e05423ec3b83c9502fdaa834b7d
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-11-04 08:58:56 -0500

    notes in js

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+// constants
 const cookieButton = document.getElementById(`cookie`)
 const winnerText = document.getElementById(`winner`)
+const resetButton = document.getElementById(`resetButton`)
 
+// hidden upon load items
 winnerText.style.display = "none"
+resetButton.style.display = "none"
 
-let count = 0;
+// variable
+let count = 0
+let random = Math.floor(Math.random() * 50) + 1;
+// syntax from stack overflow
 
+// test 
+// console.log(random)
+
+// functions
 const clickCounter = () => {
     count++;
     document.getElementById(`display`).innerText = `${count}`
-if (count === 6) {
+if (random === count) {
+    resetButton.style.display = "flex";
     winnerText.style.display = "flex"
     winnerText.style.justifyContent = "center"
     cookieButton.style.display = "none"
+    
+
 }
 }
+
+// event listeners
 
 cookieButton.addEventListener(`click`, clickCounter)
 
